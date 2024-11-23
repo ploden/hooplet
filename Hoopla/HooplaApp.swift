8d178d9f5a4cfd25be123d2a1a829b55486d8620
Phil Loden <ploden@gmail.com>
2024-11-23 16:15:42 -0700

    Initial Commit

---
+++ Hoopla/HooplaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HooplaApp.swift
+//  Hoopla
+//
+//  Created by Philip Loden on 11/23/24.
+//
+
+import SwiftUI
+
+@main
+struct HooplaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
